3f96e18d4d2037391f5c3c9b91b379cb8c56bb2c
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2023-07-21 15:19:49 -0700

    Created rough static version of the task list page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function ShowExistingTasks() {
+  return (
+    <ul>
+      <li>Features:</li>
+      <li>Add new task with title and optional description</li>
+      <li>View a list of all tasks with their defaults</li>
+      <li>Mark tasks as complete or incomplete</li>
+      <li>Edit/update exisiting tasks</li>
+      <li>Delete tasks from the list</li>
+      <li>Optional: Implement local storage to persist the tasks across sessions</li>
+    </ul>
+  );
+}
+
+function CreateNewTask() {
+  return (
+    <button>Create New Task</button>
+  );
+}
 
 function App() {
   return (
@@ -18,6 +39,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h2>My Tasks</h2>
+      <ShowExistingTasks />
+      <CreateNewTask />
     </div>
   );
 }
