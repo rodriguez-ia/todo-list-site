decc4138847e30e08a8e065fa77a0e0d1212ccae
Isaiah Rodriguez <isaiah.rod.012@gmail.com>
2023-07-22 02:22:50 -0700

    Added some styling and included date

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function ShowTaskMenu() {
+function ShowTasksMenu() {
   return (
-    <div className="App">
-      <h2>My Tasks</h2>
+    <div className="tasks-menu">
+      <h2 className='tasks-label'>My Tasks</h2>
       <ShowExistingTasks />
       <CreateNewTaskButton />
     </div>
@@ -15,13 +15,12 @@ function ShowTaskMenu() {
 function ShowExistingTasks() {
   return (
     <ul>
-      <li>Features:</li>
-      <li>Add new task with title and optional description</li>
-      <li>View a list of all tasks with their defaults</li>
-      <li>Mark tasks as complete or incomplete</li>
-      <li>Edit/update exisiting tasks</li>
-      <li>Delete tasks from the list</li>
-      <li>Optional: Implement local storage to persist the tasks across sessions</li>
+      <li><input type="checkbox" />{' '}Add new task with title and optional description</li>
+      <li><input type="checkbox" />{' '}View a list of all tasks with their defaults</li>
+      <li><input type="checkbox" />{' '}Mark tasks as complete or incomplete</li>
+      <li><input type="checkbox" />{' '}Edit/update exisiting tasks</li>
+      <li><input type="checkbox" />{' '}Delete tasks from the list</li>
+      <li><input type="checkbox" />{' '}Optional: Implement local storage to persist the tasks across sessions</li>
     </ul>
   );
 }
@@ -32,7 +31,7 @@ function CreateNewTaskButton() {
   }
 
   return (
-    <button onClick={handleClick}>Create New Task</button>
+    <button className='new-task-button' onClick={handleClick}>Create New Task</button>
   );
 }
 
@@ -53,7 +52,23 @@ function App() {
         </a>
       </header>
   */
-  return <ShowTaskMenu />;
+  let currentDate = new Date();
+  let dd = String(currentDate.getDate()).padStart(2, '0');
+  let mm = String(currentDate.getMonth() + 1).padStart(2, '0');
+  let yyyy = currentDate.getFullYear();
+  
+  currentDate = mm + '/' + dd + '/' + yyyy;
+
+  return (
+    <div>
+      <div className='navbar'>
+        <h2 className='navbar-items'>
+          {currentDate}
+        </h2>
+      </div>
+      <ShowTasksMenu />
+    </div>
+  );
 }
 
 export default App;
